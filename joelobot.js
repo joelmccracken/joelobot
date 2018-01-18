208e66338aae479fcd2bf5b9c7624cc470cfc0d8
Joel McCracken <mccracken.joel@gmail.com>
2018-01-18 13:13:55 -0500

    factor out a few functions

--- joelobot.js
+++ joelobot.js
@@ -2,54 +2,67 @@
 
 const _ = require('lodash@4.8.2');
 
-module.exports = (ctx, cb) => {
-  let data = ctx.data;
-  if(data) {
-    console.log(data);
-    if(data.type == 'url_verification'){
-      return cb(null, data.challenge);
+function messageDeleted(ctx, cb){
+  return ctx.storage.get(function(err, data){
+    if(err) {
+      return cb(err, null);
     }
-    if(data.event) {
-      var event_ts = data.event.event_ts;
+    let deletions = (data || {}).deletions || [];
+    let deletions2 = _.concat([ctx.data.event.deleted_ts], deletions);
+    let newData = _.extend({}, data, {deletions: deletions2});
+    console.log({newData: newData});
+    ctx.storage.set(
+      newData,
+      function(error){
+        return cb(error, null);
+      });
+    return null;
+  });
+}
 
-      if(data.event.subtype == 'message_deleted') {
-        return ctx.storage.get(function(err, data){
-          if(err) {
-            return cb(err, null);
-          }
-          let deletions = (data || {}).deletions || [];
-          let deletions2 = _.concat([ctx.data.event.deleted_ts], deletions);
-          let newData = _.extend({}, data, {deletions: deletions2});
-          console.log({newData: newData});
-          ctx.storage.set(
-            newData,
-            function(error){
-              return cb(error, null);
-            });
-          return null;
-        });
-      }
-      var attachments = data.event.attachments;
+function messageWithAttachment(ctx, cb) {
+  let data = ctx.data;
+  var event_ts = data.event.event_ts;
+  var attachments = data.event.attachments;
 
-      if(attachments) {
-        console.log(data);
-        ctx.storage.get(function(err, data){
-          if(err) {
-            return cb(err, null);
-          }
-          let imgs = (data || {}).imgs || {};
-          imgs[event_ts] = attachments[0].image_url;
-          console.log("imgs:", imgs);
-          ctx.storage.set({imgs: imgs}, function(error){
-            return cb(error, null);
-          });
-          return null;
-        });
-      }
-      else
-      {
-        return cb(null, "ok");
-      }
+  console.log(data);
+  ctx.storage.get(function(err, data){
+    if(err) {
+      return cb(err, null);
+    }
+    let imgs = (data || {}).imgs || {};
+    imgs[event_ts] = attachments[0].image_url;
+    console.log("imgs:", imgs);
+    ctx.storage.set({imgs: imgs}, function(error){
+      return cb(error, null);
+    });
+    return null;
+  });
+}
+
+function handleEvent(ctx, cb) {
+
+  if(ctx.data.event.subtype == 'message_deleted') {
+    return messageDeleted(ctx, cb);
+  }
+
+  if(ctx.data.event.attachments) {
+    return messageWithAttachment(ctx, cb);
+  }
+}
+
+module.exports = (ctx, cb) => {
+  if(ctx.data) {
+    console.log(ctx.data);
+    if(ctx.data.type == 'url_verification'){
+      return cb(null, ctx.data.challenge);
+    }
+    if(ctx.data.event){
+      return handleEvent(ctx,cb);
+    }
+    else
+    {
+      return cb(null, "ok");
     }
   } else {
     console.log("Error no data: ", ctx);
